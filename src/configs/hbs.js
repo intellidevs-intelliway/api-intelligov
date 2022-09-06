549a9089b2e98ac93bc98c40655aba285f2cf5f1
Anderson <Anderson@FONSECA.local>
2022-09-06 11:01:21 -0300

    Ajuste __dirname

--- src/configs/hbs.js
+++ src/configs/hbs.js
@@ -1,5 +1,9 @@
 import exphbs from "express-handlebars"
 import path from "path";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const viewPath =  path.resolve(__dirname,"..","views","emails");
 
